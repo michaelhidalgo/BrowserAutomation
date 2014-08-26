59155de4e3ea85dc1f14cc1f529b87a12d1e5dfa
Michael <michaelfalllas@gmail.com>
2014-08-25 19:11:10 -0600

    fIXING paths

--- TEAMMentor.SauceLabs.AutomationTest/Constants.cs
+++ TEAMMentor.SauceLabs.AutomationTest/Constants.cs
@@ -6,7 +6,7 @@
         /// <summary>name of the sauce labs account that will be used</summary>
         internal const string SAUCE_LABS_ACCOUNT_NAME = "mhidalgocr";
         /// <summary>account key for the sauce labs account that will be used</summary>
-        internal const string SAUCE_LABS_ACCOUNT_KEY = "a5e41e96-a3f8-4695-8677-e3af9021f606";
+        internal const string SAUCE_LABS_ACCOUNT_KEY = "73e17e46-9bc1-44f7-a46a-fed5104820b1";
 
   
 
